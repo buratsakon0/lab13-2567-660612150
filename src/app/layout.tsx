ff1a6c5ea791c2f1bd95490c2e028b70ead7dd9c
buratsakon0 <buratsakon_s@cmu.ac.th>
2024-09-03 21:18:59 +0700

    lab completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Buratsakon Srikhaw"
+            studentId="660612150"
           />
         </div>
       </body>
